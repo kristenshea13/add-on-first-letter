d863fad7aca90ce15247890341a75503ce9726ec
Kristen Shea <kstar61@live.com>
2019-04-02 18:33:48 -0500

    l

--- add on first letter/Program.cs
+++ add on first letter/Program.cs
@@ -2,11 +2,28 @@
 
 namespace add_on_first_letter
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Write a C# program to create a new string from a given string (length 1 or more ) 
+            //with the first character added at the front and back. Go to the editor
+            //Sample Output:
+            //Input a string : The quick brown fox jumps over the lazy dog.
+            //TThe quick brown fox jumps over the lazy dog.T
+
+            Console.Write("Write out a string: ");
+            string x = Console.ReadLine();
+
+
+            if (x.Length >= 1)
+            {
+                var z = x.Substring(0, 1);
+
+                Console.WriteLine($"{z}{x}{z}");
+
+            }
+            Console.ReadLine();
         }
     }
 }
